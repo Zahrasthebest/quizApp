f24fb9e8ab3c5f12d1c80cd17ce5b1af4a622ed1
Zahra <zahramoosanni@icloud.com>
2024-06-24 10:04:13 -0500

    Initial Commit

---
+++ quizApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  quizApp
+//
+//  Created by Zahra Moosani on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ quizApp/quizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizAppApp.swift
+//  quizApp
+//
+//  Created by Zahra Moosani on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct quizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
